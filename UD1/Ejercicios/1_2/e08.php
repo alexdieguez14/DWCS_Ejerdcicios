1224305d3d5e2dc751ccccc79523a71c061dad78
samuel L.C. <slcardoso.docente@gmail.com>
2025-09-30 14:12:49 +0200

    ejercicios 1.2 completos

--- UD1/Ejercicios/1_2/e08.php
+++ UD1/Ejercicios/1_2/e08.php
@@ -1,22 +1,26 @@
 <?php
 include_once "e07.php";
-class Curso{
+class Curso
+{
 
     private $estudiantes;
     private $nombre;
-    public function __construct($nombre){
+    public function __construct($nombre)
+    {
         $this->nombre = $nombre;
         $this->estudiantes = [];
     }
 
-    public function addEstudiante(Estudiante $e){
+    public function addEstudiante(Estudiante $e)
+    {
         $this->estudiantes[] = $e;
     }
 
-    public function mostrarEstudiantes(){
+    public function mostrarEstudiantes()
+    {
         $toret = '';
-        foreach($this->estudiantes as $e){
-            $toret .= "<br>".$e->mostrarInformacion();
+        foreach ($this->estudiantes as $e) {
+            $toret .= "<br>" . $e->mostrarInformacion();
         }
 
         return $toret;
@@ -27,9 +31,9 @@ class Curso{
 $d = new Direccion("plaza mayor, 3", "Vilagarcía", "36250");
 $estudiante = new Estudiante("Pedro", 20, $d, "Desarrollo Web");
 $estudiante->addCalificacion(5)
-            ->addCalificacion(7)
-            ->addCalificacion(8);
-$e2 = clone($estudiante);
+    ->addCalificacion(7)
+    ->addCalificacion(8);
+$e2 = clone ($estudiante);
 $e2->setNombre("María")
     ->setEdad(32);
 $curso = new Curso("DAW2-A");
